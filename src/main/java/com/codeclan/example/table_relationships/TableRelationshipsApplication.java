c54b11adac2c3338e0276cd959191181938221b6
Andrew Wood <andrewwood90@hotmail.com>
2021-02-24 09:10:26 +0000

    setting up repositories

---
+++ src/main/java/com/codeclan/example/table_relationships/TableRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.table_relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableRelationshipsApplication.class, args);
+	}
+
+}
